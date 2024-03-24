ad651f45957ca690012c5a2fd93421589e73167e
Nikechukwu <nikechukwu@gmail.com>
2024-03-24 16:48:36 -0400

    Import Map Changes

--- main.ts
+++ main.ts
@@ -1,25 +1,3 @@
-import { Command, EnumType } from 'https://deno.land/x/cliffy/command/mod.ts';
-import {
-    init,
-    help,
-} from "./lib/src/commands/commands.ts";
-
-const logLevel = new EnumType(['debug', 'info', 'warn', 'error']);
-
-const cli = new Command()
-.name('rex').version('0.1.0').description("Mono Repo for Deno, Node and Bun Projects")
-.globalOption('-v --verbose', 'Print verbose logging output')
-.default('init')
-.command('init', init)
-.command('check')
-.command('readme')
-.command('ci')
-.command('list')
-.command('global')
-.command('build')
-.command('publish')
-.command('help', help)
-;
+import cli from "rex-cli/src/rex_cli.ts";
 
 await cli.parse(Deno.args);
-
