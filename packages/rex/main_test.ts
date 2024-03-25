0919fd4871c842fd1b108224fd5dc47a481fb0f8
Nikechukwu <nikechukwu@gmail.com>
2024-03-25 10:06:54 -0400

    Formatting

--- packages/rex/main_test.ts
+++ packages/rex/main_test.ts
@@ -1,10 +1,14 @@
-import { assert, assertEquals, assertNotEquals } from "https://deno.land/std@0.218.0/assert/mod.ts";
+import {
+  assert,
+  assertEquals,
+  assertNotEquals,
+} from "https://deno.land/std@0.218.0/assert/mod.ts";
 import { RexFile } from "rex";
 
 Deno.test(function RexFileTest() {
-    const rfile = new RexFile("README.md", "# Hello World");
-    assertEquals(rfile.contents, "# Hello World");
-    rfile.addCode("# Rex");
-    assertNotEquals(rfile.contents, "# Hello World");
-    assertEquals(rfile.contents, "# Rex");
+  const rfile = new RexFile("README.md", "# Hello World");
+  assertEquals(rfile.contents, "# Hello World");
+  rfile.addCode("# Rex");
+  assertNotEquals(rfile.contents, "# Hello World");
+  assertEquals(rfile.contents, "# Rex");
 });
