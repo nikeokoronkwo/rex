5aed4cd0bb0ba614b1fa5f4cc56ed54348b01b05
Nikechukwu <nikechukwu@gmail.com>
2024-04-17 22:28:47 -0400

    Working on Schema Updates with publish Command

--- packages/rex-cli/src/commands/publish.ts
+++ packages/rex-cli/src/commands/publish.ts
@@ -1,4 +1,6 @@
+import { bold } from "https://deno.land/std@0.196.0/fmt/colors.ts";
 import { Command, EnumType, SEPARATOR, existsSync } from "../../deps.ts";
+import { runProcess } from "../lib/run/runProcess.ts";
 import { envType } from "../shared/env.ts";
 import { execOnRexPackages } from "../shared/execFunc.ts";
 import { runonpkgs } from "../shared/runonpkgs.ts";
@@ -10,7 +12,7 @@ const publish = new Command()
   .type("env-type", envType)
   .description(
     `
-Publish your packages in the monorepository.
+Publish your packages in the monorepository (${bold("children")} or ${bold("main")}).
 
 This command publishes the given packages inferred from the argument, using the "rex_pkg.json" or "rex.json" file to get the configuration for the publishing pipeline.
 By default cli arguments override.
@@ -51,10 +53,23 @@ function publishCommand(
 ) {
   if (args === "children") {
     execOnRexPackages((name, path) => {
-      let envs = getRexPkgEnvs(path);
+      let envs = (options.ignore ?? []).includes("all")
+        ? []
+        : getRexPkgEnvs(path, options.ignore);
+      let actions: Actions[] =
+        JSON.parse(Deno.readTextFileSync(`${path}/rex_pkg.json`)).publish
+          .actions ?? [];
       let fails = 0;
       let passes = 0;
       envs.forEach(async (env) => {
+        let pkgActions = actions.filter(
+          (e: Actions) =>
+            (e.performFor ?? ["all"]).includes(env.name) ||
+            (e.performFor ?? ["all"]).includes("all"),
+        );
+        pkgActions.forEach(async (a) => {
+          await runProcess(name, path, [a.run]);
+        });
         let { failures, successes } = await runonpkgs(
           env.name,
           path,
@@ -72,30 +87,41 @@ function publishCommand(
   }
 }
 
+type Actions = RexPkgPubActions;
+
+interface RexPkgPubActions {
+  name?: string;
+  run: string;
+  performFor: string[];
+}
+
 type envFile = {
   name: string;
   pkgName?: string;
   configPath: string;
 };
 
-function getRexPkgEnvs(path: string): envFile[] {
+function getRexPkgEnvs(path: string, ignore?: string[]): envFile[] {
   let names: envFile[] = [];
   let envExists = false;
-  if (existsSync(`${path}${SEPARATOR}deno.json`)) {
+  if (existsSync(`${path}${SEPARATOR}deno.json`) && !ignore?.includes("deno")) {
     names.push({
       name: "deno",
       configPath: `${path}${SEPARATOR}deno.json`,
     });
     envExists = true;
   }
-  if (existsSync(`${path}${SEPARATOR}jsr.json`)) {
+  if (existsSync(`${path}${SEPARATOR}jsr.json`) && !ignore?.includes("npm")) {
     names.push({
       name: "jsr",
       configPath: `${path}${SEPARATOR}jsr.json`,
     });
     envExists = true;
   }
-  if (existsSync(`${path}${SEPARATOR}package.json`)) {
+  if (
+    existsSync(`${path}${SEPARATOR}package.json`) &&
+    !ignore?.includes("npm")
+  ) {
     names.push({
       name: "npm",
       configPath: `${path}${SEPARATOR}package.json`,
