1e4f96c002f00c48e158e3ad3e583f20f5b0e579
Nikechukwu <nikechukwu@gmail.com>
2024-04-18 20:59:02 -0400

    Scripts for Rex

---
+++ packages/rex/scripts/bundle.ts
@@ -0,0 +1,15 @@
+import * as esbuild from "https://deno.land/x/esbuild@v0.20.1/mod.js";
+import { bundle } from "https://deno.land/x/emit/mod.ts";
+
+const result = await bundle("./main.ts", {
+  importMap: "../../.rexmap.json",
+});
+const { code } = result;
+Deno.writeFileSync("main.js", new TextEncoder().encode(code));
+
+// Run from project root directory
+await esbuild.build({
+  entryPoints: ["main.js"],
+  outfile: "main.min.js",
+  minify: true,
+});
