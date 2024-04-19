9bd3bedcddc9b3574c965b5b30cbda4b32090022
Nikechukwu <nikechukwu@gmail.com>
2024-04-18 22:22:06 -0400

    Reduced NPM Package Size Further

--- packages/rex-cli/deps.ts
+++ packages/rex-cli/deps.ts
@@ -2,13 +2,13 @@ export { Command } from "https://deno.land/x/cliffy/command/command.ts";
 export { EnumType } from "https://deno.land/x/cliffy/command/types/enum.ts";
 export { HelpCommand } from "https://deno.land/x/cliffy/command/help/help_command.ts";
 export { colors } from "https://deno.land/x/cliffy/ansi/colors.ts";
-export { blue, bold } from "https://deno.land/std/fmt/colors.ts";
-export { existsSync, exists } from "https://deno.land/std/fs/exists.ts";
-export { walk, walkSync } from "https://deno.land/std/fs/walk.ts";
-export type { WalkEntry } from "https://deno.land/std/fs/walk.ts";
-export { globToRegExp } from "https://deno.land/std/path/glob_to_regexp.ts";
-export { SEPARATOR } from "https://deno.land/std/path/constants.ts";
-export { normalizeGlob } from "https://deno.land/std/path/normalize_glob.ts";
+export { blue, bold } from "https://deno.land/std@0.223.0/fmt/colors.ts";
+export { existsSync, exists } from "https://deno.land/std@0.223.0/fs/exists.ts";
+export { walk, walkSync } from "https://deno.land/std@0.223.0/fs/walk.ts";
+export type { WalkEntry } from "https://deno.land/std@0.223.0/fs/walk.ts";
+export { globToRegExp } from "https://deno.land/std@0.223.0/path/glob_to_regexp.ts";
+export { SEPARATOR } from "https://deno.land/std@0.223.0/path/constants.ts";
+export { normalizeGlob } from "https://deno.land/std@0.223.0/path/normalize_glob.ts";
 export {
   RexError,
   RexConfigFile,
